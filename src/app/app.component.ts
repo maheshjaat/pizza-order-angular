efbc12ab117ba2ccbdfc758a24a8d92489cf84a5
maheshjaat <pra.mj99@gmail.com>
2022-06-19 01:26:08 +0530

    saturday commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   showModal: boolean = false;
+  toppings: any;
   vegTop: boolean = true;
   nonvTop: boolean = true;
   customizeData: any;
   customizeTopping: any;
+
+  hideme = {};
   toggleModal() {
     this.showModal = !this.showModal;
   }
 
-  order = {};
+  order = [];
   title = 'angtailwind';
   price: number = 0;
   pizza = {
@@ -106,7 +109,7 @@ export class AppComponent {
     'Cheese Burst',
     'Fresh Pan Pizza',
   ];
-  toppings = {
+  topping = {
     veg: [
       { id: 21, name: 'Black olive', price: 20 },
       { id: 22, name: 'Capsicum', price: 25 },
@@ -130,8 +133,22 @@ export class AppComponent {
   customisePizza(data: any, type: string) {
     this.showModal = !this.showModal;
     this.customizeData = data;
-    if (type === 'veg') {
+    if (type == 'veg') {
+      this.toppings = this.topping;
+      this.nonvTop = false;
     } else {
+      // this.topping.veg.splice(
+      //   this.topping.veg.findIndex((a) => a.name == 'Paneer'),
+      //   1
+      // );
+
+      // this.topping.veg.forEach(e => {
+      //   this.hideme[e.id] = false;
+      // });
+      this.toppings = this.topping;
+
+      this.vegTop = true;
+      this.nonvTop = true;
     }
     console.log(data);
   }
@@ -140,19 +157,35 @@ export class AppComponent {
     // this.pizza.veg[veg.id - 1]['price'] = event.target.value;
     console.log('price', event.target.value, veg);
   }
-  getCrustType(event: any, veg: any) {
-    // this.pizza.veg[veg.id - 1] = {
-    //   ...this.pizza.veg[veg.id - 1],
-    //   : event.target.value,
-    // };
-    this.pizza.veg[veg.id - 1].crust = event.target.value;
+  getCrustType(event: any, pizzatype: any) {
+    console.log('CrustCall');
+
+    var objIndex;
+    if (pizzatype.id < 10) {
+      objIndex = this.pizza.veg.findIndex((obj) => obj.id == pizzatype.id);
+      this.pizza.veg[objIndex].crust = event.target.value;
+      console.log('veg', objIndex);
+    } else {
+      objIndex = this.pizza.nonVeg.findIndex((obj) => obj.id == pizzatype.id);
+      this.pizza.nonVeg[objIndex].crust = event.target.value;
+      console.log('nonveg', objIndex);
+    }
+
+    console.log('crust', event, pizzatype);
   }
 
   addExtraCheese(event: any, data: any) {
+    data.price += parseInt(event.target.value);
     console.log(data, event);
   }
 
   addToCart(data: any) {
+    var mahesh = { data: '', count: '' };
+    // mahesh.data=data
+    // mahesh.count=
+
+    // this.order['jdf'] = data;
+
     console.log('data', data);
   }
 
