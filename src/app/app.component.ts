ce7f71b28f80dcacb22ae8ccfb180506e07baf63
maheshjaat <pra.mj99@gmail.com>
2022-06-19 00:12:22 +0530

    thursday commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  showModal = false;
+  showModal: boolean = false;
+  vegTop: boolean = true;
+  nonvTop: boolean = true;
   customizeData: any;
+  customizeTopping: any;
   toggleModal() {
     this.showModal = !this.showModal;
   }
+
+  order = {};
   title = 'angtailwind';
   price: number = 0;
   pizza = {
@@ -25,7 +30,6 @@ export class AppComponent {
           { size: 'large', price: 325 },
         ],
         price: 150,
-
         crust: '',
       },
       {
@@ -67,6 +71,7 @@ export class AppComponent {
           { size: 'large', price: 425 },
         ],
         crust: '',
+        price: 190,
       },
       {
         id: 12,
@@ -78,6 +83,7 @@ export class AppComponent {
           { size: 'large', price: 500 },
         ],
         crust: '',
+        price: 210,
       },
       {
         id: 13,
@@ -88,6 +94,8 @@ export class AppComponent {
           { size: 'medium', price: 380 },
           { size: 'large', price: 525 },
         ],
+        price: 220,
+        crust: '',
       },
     ],
   };
@@ -98,35 +106,38 @@ export class AppComponent {
     'Cheese Burst',
     'Fresh Pan Pizza',
   ];
-  toppings = [
-    {
-      veg: [
-        { id: 21, name: 'Black olive', price: 20 },
-        { id: 22, name: 'Capsicum', price: 25 },
-        { id: 23, name: 'Paneer', price: 35 },
-        { id: 24, name: 'Mushroom', price: 30 },
-        { id: 25, name: 'Fresh Tomato', price: 10 },
-      ],
-      nonVeg: [
-        { id: 26, name: 'Chicken Tikka', price: 35 },
-        { id: 27, name: 'Barbeque chicken', price: 45 },
-        { id: 28, name: 'Grilled chicken', price: 40 },
-      ],
-    },
-  ];
+  toppings = {
+    veg: [
+      { id: 21, name: 'Black olive', price: 20 },
+      { id: 22, name: 'Capsicum', price: 25 },
+      { id: 23, name: 'Paneer', price: 35 },
+      { id: 24, name: 'Mushroom', price: 30 },
+      { id: 25, name: 'Fresh Tomato', price: 10 },
+    ],
+    nonVeg: [
+      { id: 26, name: 'Chicken Tikka', price: 35 },
+      { id: 27, name: 'Barbeque chicken', price: 45 },
+      { id: 28, name: 'Grilled chicken', price: 40 },
+    ],
+  };
+
   slides = [
     { id: 101, name: 'Cold drink', price: 55 },
     { id: 102, name: 'Mousse cake', price: 90 },
   ];
   constructor() {}
 
-  customisePizza(data: any) {
+  customisePizza(data: any, type: string) {
     this.showModal = !this.showModal;
     this.customizeData = data;
+    if (type === 'veg') {
+    } else {
+    }
     console.log(data);
   }
   getPizzaPrice(event: any, veg: any) {
-    this.pizza.veg[veg.id - 1]['price'] = event.target.value;
+    veg.price = event.target.value;
+    // this.pizza.veg[veg.id - 1]['price'] = event.target.value;
     console.log('price', event.target.value, veg);
   }
   getCrustType(event: any, veg: any) {
@@ -135,6 +146,18 @@ export class AppComponent {
     //   : event.target.value,
     // };
     this.pizza.veg[veg.id - 1].crust = event.target.value;
+  }
+
+  addExtraCheese(event: any, data: any) {
+    console.log(data, event);
+  }
+
+  addToCart(data: any) {
+    console.log('data', data);
+  }
+
+  addTopping(event: any, toppingType: any) {
+    console.log('topp', event, toppingType);
   }
 
   ngOninit() {}
